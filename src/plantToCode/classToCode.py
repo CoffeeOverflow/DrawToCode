71b3a014e36052498809152bd81c15e81987bf1f
VitorCantao <vitornovaes.cantao@gmail.com>
2019-11-19 23:42:53 -0300

    Extract class from inheritance methods

--- src/plantToCode/classToCode.py
+++ src/plantToCode/classToCode.py
@@ -2,6 +2,7 @@ from src.plantToCode.classData import ClassData
 from src.plantToCode.modifier import Modifier
 from src.plantToCode.methodToCode import MethodToCode
 from src.plantToCode.interfaceToCode import InterfaceToCode
+from src.plantToCode.inheritanceToCode import InheritanceToCode
 
 
 class ClassToCode:
@@ -9,6 +10,7 @@ class ClassToCode:
     def __init__(self, class_data: ClassData):
         self.class_data = class_data
         self.method_to_code = MethodToCode(self.class_data.methods)
+        self.inheritance_to_code = InheritanceToCode(self.class_data.inheritances)
 
     def convert(self) -> str:
         return (f"{self.__formatted_class_header()}"
@@ -19,20 +21,9 @@ class ClassToCode:
     def __formatted_class_header(self):
         return (f"{self.class_data.visibility.name} {self.class_data.modifier.value}"
                 f"{'' if self.class_data.modifier is Modifier.none else ' '}"
-                f"class {self.class_data.name}{self.__get_optional_inheritances()}"
+                f"class {self.class_data.name}{self.inheritance_to_code.get_formatted()}"
                 f"{InterfaceToCode.codeImplementedInterfaces(self.class_data.implementations)}"
                 f" {{\n")
-
-    def __get_optional_inheritances(self) -> str:
-        if self.class_data.inheritances:
-            return f" extends {self.__formatted_inheritances()}"
-        else:
-            return ''
-
-    def __formatted_inheritances(self) -> str:
-        inheritance_list = [f"{class_data.name}"
-                            for class_data in self.class_data.inheritances]
-        return ', '.join(inheritance_list)
 
     def __formatted_fields(self):
         class_fields = [f"\t{fields.visibility.value} {fields.type_} {fields.name};"
---
+++ src/plantToCode/inheritanceToCode.py
@@ -0,0 +1,17 @@
+from src.plantToCode.classData import ClassData
+
+
+class InheritanceToCode:
+    def __init__(self, inheritances: [ClassData]):
+        self.inheritances = inheritances
+
+    def get_formatted(self) -> str:
+        if self.inheritances:
+            return f" extends {self.__formatted_names()}"
+        else:
+            return ''
+
+    def __formatted_names(self) -> str:
+        inheritance_list = [f"{class_data.name}"
+                            for class_data in self.inheritances]
+        return ', '.join(inheritance_list)
