f73c86193f952295ed551b0f6e6f86f379917adb
VitorCantao <vitornovaes.cantao@gmail.com>
2019-12-02 21:10:30 -0300

    Fixes #22

--- src/plantToCode/dataToCode/methodToCode.py
+++ src/plantToCode/dataToCode/methodToCode.py
@@ -5,15 +5,23 @@ from typing import List
 
 
 class MethodToCode:
-    def __init__(self, methods: List[Method]):
+    def __init__(self, methods: List[Method], is_from_interface: bool = False):
         self.methods = methods
+        self.is_from_interface = is_from_interface
 
     def __convert_method(self, method: Method) -> str:
         return (f"{method.visibility.name} "
                 f"{method.modifier.value}"
                 f"{'' if method.modifier is Modifier.none else ' '}"
                 f"{method.return_type} {method.name}"
-                f"({self.__formatted_parameters(method.parameters)})")
+                f"({self.__formatted_parameters(method.parameters)})"
+                f"{self.__formatted_body()}")
+
+    def __formatted_body(self) -> str:
+        if self.is_from_interface:
+            return ";"
+        else:
+            return " {\n\t\tthrow new UnsupportedOperationException();\n\t}"
 
     def __formatted_parameters(self, parameters: List[Attribute]) -> str:
         parameters = [f"{parameter.type_} {parameter.name}"
@@ -21,6 +29,6 @@ class MethodToCode:
         return ', '.join(parameters)
 
     def get_formatted_methods(self) -> str:
-        methods_str_list = [f"\t{self.__convert_method(method)};"
+        methods_str_list = [f"\t{self.__convert_method(method)}"
                             for method in self.methods]
         return '\n\n'.join(methods_str_list)
