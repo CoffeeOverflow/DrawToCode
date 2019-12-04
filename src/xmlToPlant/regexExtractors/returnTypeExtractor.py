9da09110137dffeed677cee7520f42f2c21b34a0
Pleonazi <aianshay@gmail.com>
2019-12-04 20:55:43 -0300

    Update returnTypeExtractor.py
    Delete not so useful print

--- src/xmlToPlant/regexExtractors/returnTypeExtractor.py
+++ src/xmlToPlant/regexExtractors/returnTypeExtractor.py
@@ -7,7 +7,7 @@ class ReturnTypeExtractor:
     def extract_type(xml_string) -> str:
         regex = re.compile('\)*:\s*\w+')
         list_of_types = regex.findall(xml_string)
-        # print(list_of_types)
+
         if ')' in list_of_types[-1]:
             return list_of_types[-1].split(':')[1].lstrip()
 
