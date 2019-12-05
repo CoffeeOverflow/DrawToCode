d6838ae7fa4038f463c70d9ec16ba80c90c81f97
Pedro <ppvitorr@gmail.com>
2019-12-05 14:03:28 -0300

    Update parametersExtractor.py

--- src/xmlToPlant/regexExtractors/parametersExtractor.py
+++ src/xmlToPlant/regexExtractors/parametersExtractor.py
@@ -5,7 +5,7 @@ class ParametersExtractor:
 
     @staticmethod
     def extract_parameters_string(xml_string) -> list:
-        regex = re.compile('\(.*\)')
+        regex = re.compile(r"\(.*\)")
         name = str(regex.findall(xml_string))
         unformatted_parameters_string_list = name[3:-3].split(",")
         formatted_parameters_string_list = []
