50b0c9b918da07b3350efe7ae3bc5c923c64584e
Pedro <ppvitorr@gmail.com>
2019-12-05 13:06:46 -0300

    Update Test_visibilityExtractor.py

--- test/test_xmlToPlant/test_regexExtractors/test_visibilityExtractor.py
+++ test/test_xmlToPlant/test_regexExtractors/test_visibilityExtractor.py
@@ -3,13 +3,38 @@ from src.plantToCode.visibility import Visibility
 from src.xmlToPlant.regexExtractors.visibilityExtractor import VisibilityExtractor
 
 
-@pytest.mark.parametrize("xml_string", ["+ age: int"])
-def test_extract_visibility_from_attribute(xml_string):
+@pytest.mark.parametrize("xml_string", ["+ age: int", "+        weight: float",
+                                        "     +      height: double"])
+def test_extract_public_visibility_from_attributes(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
-    assert visibility in [Visibility.public, Visibility.private, Visibility.protected]
+    assert visibility == Visibility.public
 
-
-@pytest.mark.parametrize("xml_string", ["# get_age(): int"])
-def test_extract_visibility_from_method(xml_string):
+@pytest.mark.parametrize("xml_string", ["- age: int", "-        weight: float",
+                                        "     -      height: double"])
+def test_extract_private_visibility_from_attributes(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
-    assert visibility in [Visibility.public, Visibility.private, Visibility.protected]
+    assert visibility == Visibility.private
+
+@pytest.mark.parametrize("xml_string", ["# age: int", "#        weight: float",
+                                        "     #      height: double"])
+def test_extract_protected_visibility_from_attributes(xml_string):
+    visibility = VisibilityExtractor.extract_visibility(xml_string)
+    assert visibility == Visibility.protected
+
+@pytest.mark.parametrize("xml_string", ["+ get_age(): int", "+        add_weight(float additional_wight): float",
+                                        "     +      set_height(double height): void"])
+def test_extract_public_visibility_from_methods(xml_string):
+    visibility = VisibilityExtractor.extract_visibility(xml_string)
+    assert visibility in [Visibility.protected, Visibility.public, Visibility.private]
+
+@pytest.mark.parametrize("xml_string", ["- get_age(): int", "-        add_weight(float additional_wight): float",
+                                        "     -      set_height(double height): void"])
+def test_extract_private_visibility_from_methods(xml_string):
+    visibility = VisibilityExtractor.extract_visibility(xml_string)
+    assert visibility in [Visibility.protected, Visibility.public, Visibility.private]
+
+@pytest.mark.parametrize("xml_string", ["# get_age(): int", "#        add_weight(float additional_wight): float",
+                                        "     #      set_height(double height): void"])
+def test_extract_protected_visibility_from_methods(xml_string):
+    visibility = VisibilityExtractor.extract_visibility(xml_string)
+    assert visibility in [Visibility.protected, Visibility.public, Visibility.private]
