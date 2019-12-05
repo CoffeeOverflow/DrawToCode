dfcd4b9529efb7b24335e5822388498329e7debd
Pedro <ppvitorr@gmail.com>
2019-12-05 14:03:07 -0300

    Update methodNameExtractor.py

--- src/xmlToPlant/regexExtractors/methodNameExtractor.py
+++ src/xmlToPlant/regexExtractors/methodNameExtractor.py
@@ -5,6 +5,6 @@ class MethodNameExtractor:
 
     @staticmethod
     def extract_name(xml_string) -> str:
-        regex = re.compile('\w+\(*.*:')
+        regex = re.compile(r"\w+\(*.*:")
         name = str(regex.findall(xml_string))
         return str(name[2:-3].split('(')[0])
