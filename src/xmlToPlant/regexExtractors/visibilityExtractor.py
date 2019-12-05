cb8bc319584e8b7930e40530e9d715cc485a657e
Pedro <ppvitorr@gmail.com>
2019-12-05 12:55:15 -0300

    Update visibilityExtractor.py

--- src/xmlToPlant/regexExtractors/visibilityExtractor.py
+++ src/xmlToPlant/regexExtractors/visibilityExtractor.py
@@ -7,6 +7,6 @@ class VisibilityExtractor:
     @staticmethod
     def extract_visibility(xml_string) -> Visibility:
         types_of_visibilities = {"+": Visibility.public, "-": Visibility.private, "#": Visibility.protected}
-        regex = re.compile('^.')
-        visibility = str(regex.findall(xml_string))
-        return types_of_visibilities[visibility[2:-2]]
+        regex = re.compile('^\s*.')
+        visibility = str(regex.findall(xml_string))[2:-2]
+        return types_of_visibilities[visibility.strip()]
