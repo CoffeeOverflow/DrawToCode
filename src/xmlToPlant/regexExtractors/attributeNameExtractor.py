211e5f652840a89e5c781738fd7f62c1b6fd6085
Pedro <ppvitorr@gmail.com>
2019-12-05 14:02:30 -0300

    Update attributeNameExtractor

--- src/xmlToPlant/regexExtractors/attributeNameExtractor.py
+++ src/xmlToPlant/regexExtractors/attributeNameExtractor.py
@@ -5,6 +5,6 @@ class AttributeNameExtractor:
 
     @staticmethod
     def extract_name(xml_string) -> str:
-        regex = re.compile('\w+\(*.*:')
+        regex = re.compile(r"\w+\(*.*:")
         name = str(regex.findall(xml_string))
         return name[2:-3]
