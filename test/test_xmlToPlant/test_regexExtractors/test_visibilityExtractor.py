2936d1d8ef00611444c5445416cafd7207fd20a7
Pedro Melo <ppvitorr@gmail.com>
2019-12-06 01:47:26 -0300

    Update test_visibilityExtractor.py

--- test/test_xmlToPlant/test_regexExtractors/test_visibilityExtractor.py
+++ test/test_xmlToPlant/test_regexExtractors/test_visibilityExtractor.py
@@ -9,11 +9,13 @@ def test_extract_public_visibility_from_attributes(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
     assert visibility == Visibility.public
 
+
 @pytest.mark.parametrize("xml_string", ["- age: int", "-        weight: float",
                                         "     -      height: double"])
 def test_extract_private_visibility_from_attributes(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
     assert visibility == Visibility.private
+
 
 @pytest.mark.parametrize("xml_string", ["# age: int", "#        weight: float",
                                         "     #      height: double"])
@@ -21,17 +23,20 @@ def test_extract_protected_visibility_from_attributes(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
     assert visibility == Visibility.protected
 
+
 @pytest.mark.parametrize("xml_string", ["+ get_age(): int", "+        add_weight(float additional_wight): float",
                                         "     +      set_height(double height): void"])
 def test_extract_public_visibility_from_methods(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
     assert visibility in [Visibility.protected, Visibility.public, Visibility.private]
 
+
 @pytest.mark.parametrize("xml_string", ["- get_age(): int", "-        add_weight(float additional_wight): float",
                                         "     -      set_height(double height): void"])
 def test_extract_private_visibility_from_methods(xml_string):
     visibility = VisibilityExtractor.extract_visibility(xml_string)
     assert visibility in [Visibility.protected, Visibility.public, Visibility.private]
+
 
 @pytest.mark.parametrize("xml_string", ["# get_age(): int", "#        add_weight(float additional_wight): float",
                                         "     #      set_height(double height): void"])
