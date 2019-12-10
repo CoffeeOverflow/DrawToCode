f3986c384ca919a1720c8ae6e2e7e60f4252655a
Renan Cunha <renancunhafonseca@gmail.com>
2019-12-10 13:58:42 -0300

    add __eq__ method in ClassData

--- src/plantToCode/dataClasses/classData.py
+++ src/plantToCode/dataClasses/classData.py
@@ -22,3 +22,17 @@ class ClassData:
         self.implementations = implementations
         self.visibility = visibility
         self.modifier = modifier
+
+    def __eq__(self, other: "ClassData"):
+        cond1 = self.name == other.name
+        cond2 = self.fields == other.fields
+        cond3 = self.methods == other.methods
+        cond4 = self.inheritances == other.inheritances
+        cond5 = self.implementations == other.implementations
+        cond6 = self.visibility == other.visibility
+        cond7 = self.modifier == other.modifier
+
+        if cond1 and cond2 and cond3 and cond4 and cond5 and cond6 and cond7:
+            return True
+        else:
+            return False
