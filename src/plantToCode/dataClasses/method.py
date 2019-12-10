7097200e13f8ac4f887ac9da4cf52aca9ff149d4
Renan Cunha <renancunhafonseca@gmail.com>
2019-12-10 13:34:47 -0300

    add __eq__ method in Method class

--- src/plantToCode/dataClasses/method.py
+++ src/plantToCode/dataClasses/method.py
@@ -16,3 +16,20 @@ class Method:
         self.parameters = parameters
         self.visibility = visibility
         self.modifier = modifier
+
+    def __eq__(self, other: "Method"):
+        cond1 = self.name == other.name
+        cond2 = self.return_type == other.return_type
+        cond3 = self.visibility == other.visibility
+        cond4 = self.modifier == other.modifier
+        cond5 = len(self.parameters) == len(other.parameters)
+        
+        if not(cond1 and cond2 and cond3 and cond4 and cond5):
+            return False
+        print('ok')
+        for x, y in zip(self.parameters, other.parameters):
+            if x != y:
+                return False
+        
+        return True
+
