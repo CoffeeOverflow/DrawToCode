6731d5aed88f23fb306d36c78fe9823004a201ec
Pedro Melo <ppvitorr@gmail.com>
2019-12-14 21:46:09 -0300

    Update interfacesParser.py to fix bug

--- src/xmlToPlant/interfaceParser.py
+++ src/xmlToPlant/interfaceParser.py
@@ -17,6 +17,7 @@ class InterfaceParser:
     @staticmethod
     def read_xml(uml_data: str):
         list_of_methods = []
+        list_of_methods_string_literals = []
 
         html = bs(uml_data, 'html.parser')
 
@@ -24,28 +25,32 @@ class InterfaceParser:
 
         interface_name = result[0].b.string
 
-        list_of_methods_string_literals = str(result[1]).split("<br/>")
+        list_of_methods_string_literals = list_of_methods_string_literals + str(result[-1]).split("<br>")
+        if len(list_of_methods_string_literals) == 1:
+            list_of_methods_string_literals = str(result[-1]).split("<br/>")
+
         list_of_methods_string_literals[0] = \
             list_of_methods_string_literals[0].replace("<p style=\"margin: 0px ; margin-left: 4px\">", "")
         list_of_methods_string_literals[-1] = \
             list_of_methods_string_literals[-1].replace("</p>", "")
 
         for xml_string in list_of_methods_string_literals:
-            visibility = VisibilityExtractor.extract_visibility(xml_string)
+            if xml_string != "":
+                visibility = VisibilityExtractor.extract_visibility(xml_string)
 
-            type_ = ReturnTypeExtractor.extract_type(xml_string)
-            name = MethodNameExtractor.extract_name(xml_string)
-            list_of_parameters_string = ParametersExtractor.extract_parameters_string(xml_string)
+                type_ = ReturnTypeExtractor.extract_type(xml_string)
+                name = MethodNameExtractor.extract_name(xml_string)
+                list_of_parameters_string = ParametersExtractor.extract_parameters_string(xml_string)
 
-            list_of_parameters = []
-            if len(list_of_parameters_string) != 0 and list_of_parameters_string[0] != '':
-                for parameter_string in list_of_parameters_string:
-                    parameter_name = AttributeNameExtractor.extract_name(parameter_string)
-                    parameter_type = ReturnTypeExtractor.extract_type(parameter_string)
-                    parameter = Attribute(parameter_name, parameter_type)
-                    list_of_parameters.append(parameter)
+                list_of_parameters = []
+                if len(list_of_parameters_string) != 0 and list_of_parameters_string[0] != '':
+                    for parameter_string in list_of_parameters_string:
+                        parameter_name = AttributeNameExtractor.extract_name(parameter_string)
+                        parameter_type = ReturnTypeExtractor.extract_type(parameter_string)
+                        parameter = Attribute(parameter_name, parameter_type)
+                        list_of_parameters.append(parameter)
 
-            method = Method(name, type_, list_of_parameters, visibility)
-            list_of_methods.append(method)
+                method = Method(name, type_, list_of_parameters, visibility)
+                list_of_methods.append(method)
 
         return Interface(interface_name, list_of_methods)
--- test/test_xmlToPlant/test_drawIoXmlParser.py
+++ test/test_xmlToPlant/test_drawIoXmlParser.py
@@ -30,12 +30,14 @@ def test_classes_returned_read_xml():
     list_of_classes, list_of_interfaces = DrawIoXmlParser(examples_folder).read_xml()
     assert list_of_classes[0] == class_
 
+
 def test_extract_value_from_cells():
     examples_folder = path.abspath(path.join(__file__, "../../../src/xmlToPlant/uml_samples/uml_interface.xml"))
     xml = ET.parse(examples_folder)
     root = xml.getroot()
     list_of_ids = ['-cuIEp4vuOl5B3aBU6dm-1']
-    list_of_xml_classes = ["<p style=\"margin: 0px ; margin-top: 4px ; text-align: center\"><i>&lt;&lt;Interface&gt;&gt;</i><br><b>CalculoDeSalario</b></p><hr size=\"1\"><p style=\"margin: 0px ; margin-left: 4px\">+ calcular_salario_1(funcionario: Funcionario): float<br>+ calcular_salario_2(funcionario: Funcionario, carga_horaria: int): float</p>"]
+    list_of_xml_classes = [
+        "<p style=\"margin: 0px ; margin-top: 4px ; text-align: center\"><i>&lt;&lt;Interface&gt;&gt;</i><br><b>CalculoDeSalario</b></p><hr size=\"1\"><p style=\"margin: 0px ; margin-left: 4px\">+ calcular_salario_1(funcionario: Funcionario): float<br>+ calcular_salario_2(funcionario: Funcionario, carga_horaria: int): float</p>"]
     list_of_xml_classes_, list_of_ids_, superclass_to_subclasses, implements_dict = \
         DrawIoXmlParser.extract_value_from_cells(root)
     assert implements_dict == {}
@@ -44,3 +46,25 @@ def test_extract_value_from_cells():
     assert list_of_ids == list_of_ids_
 
 
+def test():
+    examples_folder = path.abspath(path.join(__file__, "../../../src/xmlToPlant/uml_samples/orcs.xml"))
+    a = DrawIoXmlParser(examples_folder)
+    class_list, interface_list = a.read_xml()
+
+    for class_ in class_list:
+        print(class_.name)
+        for attribute in class_.fields:
+            print(attribute.visibility, attribute.name, attribute.type_)
+        for method in class_.methods:
+            print(method.visibility, method.name, method.return_type)
+            for parameter in method.parameters:
+                print(parameter.name, parameter.type_)
+        print()
+
+    for interface_ in interface_list:
+        print(interface_.name)
+        for method in interface_.methods:
+            print(method.visibility, method.name, method.return_type)
+            for parameter in method.parameters:
+                print(parameter.name, parameter.type_)
+        print()
