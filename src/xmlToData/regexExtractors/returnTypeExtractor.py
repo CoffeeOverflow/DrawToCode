f6597104389cfbabab608014911a661215854cb1
Pedro <ppvitorr@gmail.com>
2019-12-17 14:34:23 -0300

    Update returnTypeExtractor.py to fix Renan's bug

--- src/xmlToData/regexExtractors/returnTypeExtractor.py
+++ src/xmlToData/regexExtractors/returnTypeExtractor.py
@@ -5,18 +5,10 @@ class ReturnTypeExtractor:
 
     @staticmethod
     def extract_type(xml_string) -> str:
-        regex = re.compile(r"\)*:\s*.+")
+        regex = re.compile(r"\)*:\s*\w+\<*\w*\>*")
         list_of_types = regex.findall(xml_string)
 
         if ')' in list_of_types[-1]:
-            for i in range(len(list_of_types)):
-                list_of_types[i] = list_of_types[i].replace("&gt;", ">")
-                list_of_types[i] = list_of_types[i].replace("&lt;", "<")
+            return list_of_types[-1].split(':')[1].lstrip()
 
-            return list_of_types[-1].split(':')[1].strip()
-
-        for i in range(len(list_of_types)):
-            list_of_types[i] = list_of_types[i].replace("&gt;", ">")
-            list_of_types[i] = list_of_types[i].replace("&lt;", "<")
-
-        return list_of_types[-1][1:].strip()
+        return list_of_types[-1][1:].lstrip()
