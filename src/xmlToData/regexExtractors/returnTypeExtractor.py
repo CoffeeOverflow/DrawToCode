5cc97f2709405fe520fed4c9d895503dcddf826b
Pleonazi <aianshay@gmail.com>
2019-12-17 15:33:39 -0300

    Remove print

--- src/xmlToData/regexExtractors/returnTypeExtractor.py
+++ src/xmlToData/regexExtractors/returnTypeExtractor.py
@@ -10,7 +10,6 @@ class ReturnTypeExtractor:
 
         regex = re.compile(r"\)*:\s*\w+\<*\w*\>*")
         list_of_types = regex.findall(xml_string)
-        print(list_of_types)
 
         if ')' in list_of_types[-1]:
             return list_of_types[-1].split(':')[1].lstrip()
